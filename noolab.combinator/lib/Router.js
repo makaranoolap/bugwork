ff9bb2ce10e8788c457ee7be2730d10e338c3fcd
makaranoolap <makra.prum@gmail.com>
2016-01-13 14:38:29 +0700

    commit change

--- noolab.combinator/lib/Router.js
+++ noolab.combinator/lib/Router.js
@@ -2,7 +2,7 @@ Router.configure({
     layoutTemplate: 'mainLayout',
     
 });
-<<<<<<< HEAD
+
 
 Router.route('/header', {
     name: 'header'
@@ -11,9 +11,6 @@ Router.route('/footer', {
     name: 'footer'
 });
 
-
-=======
 Router.route('/login',{
 	name: 'login'
 });
->>>>>>> 70a2c4fa0cbcdf4969ae58d1b4026f7d1be97386
